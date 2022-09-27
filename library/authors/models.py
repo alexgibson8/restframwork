a8c9563515d748f0ad7521f44575c2c6469251d4
Alex Kozlov <aleksandr.21k@mail.ru>
2022-09-27 16:52:13 +0500

    123

--- library/authors/models.py
+++ library/authors/models.py
@@ -5,6 +5,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     birthday_year = models.PositiveIntegerField()
+    email = models.EmailField(unique=True)
 
 
     def __str__(self):
--- library/authors/views.py
+++ library/authors/views.py
@@ -5,6 +5,7 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import AuthorModelSerializer
 from .models import Author
 
+
 class AuthorModelViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
