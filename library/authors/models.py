d4f7eb481e4842b3e87ba64e11cab886602363b0
Alex Kozlov <aleksandr.21k@mail.ru>
2022-09-27 16:55:46 +0500

    1234

--- library/authors/models.py
+++ library/authors/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Author(models.Model):
     first_name = models.CharField(max_length=64)
@@ -7,6 +8,5 @@ class Author(models.Model):
     birthday_year = models.PositiveIntegerField()
     email = models.EmailField(unique=True)
 
-
     def __str__(self):
-        return  f'{self.last_name}-{self.first_name}'
+        return f'{self.last_name}-{self.first_name}'
